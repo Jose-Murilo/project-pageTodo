6cb7bfcb14e42890bf4cb03798c7a0299a8bf3a0
Jose Murilo <murilojucas360@hotmail.com>
2023-04-13 17:05:14 -0300

    add react query

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -3,15 +3,19 @@ import { defaultTheme } from '../styles/themes/default'
 import { GlobalStyle } from '../styles/resetGlobal'
 import { Router } from '../routes'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '../services/QueryClient'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-      <GlobalStyle />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={defaultTheme}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+        <GlobalStyle />
+      </ThemeProvider>
+    </QueryClientProvider>
   )  
 }
 
