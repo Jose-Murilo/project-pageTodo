be254979c2c20e25a458d4915fb4405468a0212c
Jose Murilo <murilojucas360@hotmail.com>
2023-04-14 21:23:28 -0300

    delete line

--- client/src/components/Menu/index.tsx
+++ client/src/components/Menu/index.tsx
@@ -1,29 +1,28 @@
 import { NavLink } from 'react-router-dom';
 import { NavContainer } from './style';
 interface MenuProps {
-    isToggle: boolean;
-    onSetToggle: (toggle: boolean) => void;
+  isToggle: boolean;
+  onSetToggle: (toggle: boolean) => void;
 }
 
 export function Menu({ isToggle, onSetToggle }: MenuProps) {
-    function toggle() {
-        onSetToggle(false)
-    }
-    
-    return (
-        // <nav className={isToggle ? styles.navContainer : styles.navFalse}>
-        <NavContainer isToggle={isToggle}>
-            <ul className='ulToggle'>
-            <li>
-              <NavLink onClick={toggle} className={(NavLink) => NavLink.isActive ? 'active' : '' } to="/">HOME</NavLink>
-            </li>
-            <li>
-              <NavLink onClick={toggle} className={(NavLink) => NavLink.isActive ? 'active' : '' } to="/tasks">TASKS</NavLink>
-            </li>
-            <li>
-              <NavLink onClick={toggle} className={(NavLink) => NavLink.isActive ? 'active' : '' } to="/about">ABOUT</NavLink>
-            </li>
-            </ul>
-        </NavContainer>
-    )
+  function toggle() {
+    onSetToggle(false)
+  }
+
+  return (
+    <NavContainer isToggle={isToggle}>
+      <ul className='ulToggle'>
+        <li>
+          <NavLink onClick={toggle} className={(NavLink) => NavLink.isActive ? 'active' : ''} to="/">HOME</NavLink>
+        </li>
+        <li>
+          <NavLink onClick={toggle} className={(NavLink) => NavLink.isActive ? 'active' : ''} to="/tasks">TASKS</NavLink>
+        </li>
+        <li>
+          <NavLink onClick={toggle} className={(NavLink) => NavLink.isActive ? 'active' : ''} to="/about">ABOUT</NavLink>
+        </li>
+      </ul>
+    </NavContainer>
+  )
 }
