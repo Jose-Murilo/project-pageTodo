2414b110c6c8528f4ed338ba30d1a1b68bbbb32c
Jose Murilo <murilojucas360@hotmail.com>
2023-04-18 17:08:48 -0300

    update in Tasks

--- client/src/pages/Tasks/index.tsx
+++ client/src/pages/Tasks/index.tsx
@@ -1,12 +1,15 @@
 // import { useQuery } from "@tanstack/react-query"
 
-import { useEffect } from 'react'
+import { useEffect, useContext } from 'react'
 import Empty from '../../assets/Empty.svg'
-import { Container, EmptyText, Modal } from "./style";
+import { Container, EmptyText } from "./style";
 import { BsTrash3Fill } from 'react-icons/bs'
 import { API } from "../../services/api";
 import { useState } from "react";
 import { InputTask } from "../../components/InputTask";
+import { TaskContext } from '../../context';
+import { Modal } from '../../components/Modal';
+import { CardTask } from '../../components/CardTask';
 export interface DataProps {
     id: number;
     titleTask: string
@@ -14,21 +17,15 @@ export interface DataProps {
 }[]
 
 export function Tasks() {
-    const [tasks, setTasks] = useState<number>(0)
-    const [showModal, setShowModal] = useState(false)
-    const [taskData, setTaskData] = useState({})
     const [data, setData] = useState<DataProps[]>([])
-    
+
+    const { setTaskData, setShowModal } = useContext(TaskContext)
+
     async function getTasks() {
         const response = await API.get('')
         const data = response.data
         setData(data)
     }
-
-    // const { data } = useQuery<DataProps[]>({
-    //     queryKey: ["tasks", tasks],
-    //     queryFn: getTasks
-    // })
 
     useEffect(() => {
         getTasks()
@@ -39,15 +36,12 @@ export function Tasks() {
         return response
     }
 
-    const modalOpen = (TaskID: number) =>  { 
+    const modalOpen = (TaskID: number) => {
         setShowModal(true)
-        const task = data?.findIndex(task => task.id == TaskID);
-        getTasks()
-        setTaskData(data[task]);
+        const taskIndex = data?.findIndex(task => task.id == TaskID);
+        return setTaskData(data[taskIndex]);
     };
 
-    // console.log(taskData);
-    
     return (
         <Container>
             <div className="containerCard">
@@ -58,50 +52,31 @@ export function Tasks() {
                         <div className="containerTask">
                             {data.map(task => {
                                 return (
-                                    <div className="cardTask">
-                                        <div className="containerTitle">
-                                            <h2 key={task.id}>
-                                                {
-                                                    task.titleTask.length > 17 ? task.titleTask.substring(0, 17) + '...' : task.titleTask
-                                                }
-                                            </h2>
-                                        </div>
-                                        <p className="descriptions">{task.descriptionTask}</p>
-
-                                        <div className="viewMore">
-                                            <button onClick={() => {
-                                                modalOpen(task.id)
-                                            }
-                                            } className="buttonViewMore">Ver +</button>
-                                        </div>
-
-                                        <BsTrash3Fill className="trashTask" onClick={() => {
-                                            deleteTasks(task.id)
-                                            setTasks(tasks + 1)
-                                        }} />
-                                    </div>
+                                    <>
+                                        <CardTask 
+                                            key={task.id}
+                                            task={task} 
+                                            modalOpen={modalOpen} 
+                                            deleteTasks={deleteTasks}
+                                        />
+                                    </>
                                 )
                             })}
                         </div>
 
-                        <Modal modal={showModal}>
-                            <div onClick={() => setShowModal(false)} className="backdrop"></div>
-                            <div className="modal-content">
-                                <h1>Alterar Tarefas</h1>
-
-                                <InputTask taskData={taskData} titleTask="Alterar titulo da tarefa" descriptionTask="Alterar descrição da tarefa" />
-                            </div>
-                        </Modal>
+                        <Modal />
                     </>
                 }
 
-                {data?.length == 0 && (
-                    <div className="containerEmpty">
-                        <img src={Empty} alt="" />
-                        <EmptyText className="emptyTasks">Você ainda não tem tarefas cadastradas</EmptyText>
-                        <EmptyText>Crie tarefas e organize seus itens a fazer</EmptyText>
-                    </div>
-                )}
+                {
+                    data?.length == 0 && (
+                        <div className="containerEmpty">
+                            <img src={Empty} alt="" />
+                            <EmptyText className="emptyTasks">Você ainda não tem tarefas cadastradas</EmptyText>
+                            <EmptyText>Crie tarefas e organize seus itens a fazer</EmptyText>
+                        </div>
+                    )
+                }
             </div>
         </Container>
     )
