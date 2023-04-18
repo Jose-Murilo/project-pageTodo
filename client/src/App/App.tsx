c9a7fb508bcf806b5aafe2bca15276ba773e36b4
Jose Murilo <murilojucas360@hotmail.com>
2023-04-17 22:09:59 -0300

    add context in app

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -5,18 +5,21 @@ import { Router } from '../routes'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '../services/QueryClient'
+import { ContextProvider } from '../context'
 
 export function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={defaultTheme}>
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
-        <GlobalStyle />
-      </ThemeProvider>
-    </QueryClientProvider>
-  )  
+    <ContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={defaultTheme}>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+          <GlobalStyle />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </ContextProvider>
+  )
 }
 
 
