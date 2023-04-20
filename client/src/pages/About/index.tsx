5ef52be261b71b23f465590468e882d1b21da30b
Jose Murilo <murilojucas360@hotmail.com>
2023-04-20 17:15:13 -0300

    Add list of project uses

--- client/src/pages/About/index.tsx
+++ client/src/pages/About/index.tsx
@@ -1,5 +1,29 @@
 export function About() {
     return (
-        <h1>About</h1>
+        <div>
+            <h1>Sobre</h1>
+
+            <h2>Front-End</h2>
+            <ul>
+                <li>React</li>
+                <li>React Icons</li>
+                <li>TypeScript</li>
+                <li>Styled-Components</li>
+                <li>React-Router</li>
+                <li>React-Hook-Form</li>
+            </ul>
+
+            <h2>Back-End</h2>
+            <ul>
+                <li>NodeJs</li>
+                <li>Express</li>
+                <li>Prisma</li>
+                <li>TypeScript</li>
+                <li>Cors</li>
+                <li>Nodemon</li>
+            </ul>
+
+            <p>Sistema desenvolvido by @Murilo & @Italo & @Jill</p>
+        </div>
     )
 }
