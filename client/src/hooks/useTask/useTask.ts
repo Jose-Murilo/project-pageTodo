925d22afedc3d169713900ef9755987ff7e51e33
Jose Murilo <murilojucas360@hotmail.com>
2023-04-27 00:11:28 -0300

    add hook useTask

---
+++ client/src/hooks/useTask/useTask.ts
@@ -0,0 +1,38 @@
+import { useState, useContext, useEffect } from "react"
+import { TaskContext } from "../../context"
+
+export function useTask() {
+    const [searchTask, setSearchTask] = useState<string>('')
+    const { setTaskData, setShowModal, showModal, fetchTasks, tasks } = useContext(TaskContext)
+    const noTask = tasks?.length === 0
+
+    const filterTask = tasks.filter(task => {
+        return (
+            task.titleTask.toLowerCase().includes(searchTask.toLowerCase()) ||
+            task.descriptionTask.toLowerCase().includes(searchTask.toLowerCase())
+        )
+    })
+
+    const noTaskFound = searchTask && filterTask.length === 0
+
+    useEffect(() => {
+        fetchTasks()
+    }, [])
+
+    const modalOpen = (TaskID: number) => {
+        setShowModal(true)
+        const taskIndex = tasks?.findIndex(task => task.id == TaskID);
+        return setTaskData(tasks[taskIndex]);
+    };
+
+    return {
+        searchTask,
+        setSearchTask,
+        showModal,
+        tasks,
+        noTask,
+        filterTask,
+        noTaskFound,
+        modalOpen
+    }
+}
