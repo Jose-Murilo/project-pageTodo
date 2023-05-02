59f223ba660e3739f3e08aa188c46fddc93112bf
Jose Murilo <murilojucas360@hotmail.com>
2023-05-02 13:47:42 -0300

    add comment in DefaultLayout

--- client/src/layout/DefaultLayout.tsx
+++ client/src/layout/DefaultLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header";
 
+// DefaultLayout é um layout padrão para todas as rotas da aplicação
 export function DefaultLayout() {
     return (
         <>
