9325299eb02f9739903ba213773d838d986b88d8
Jose Murilo <murilojucas360@hotmail.com>
2023-05-01 23:59:58 -0300

    add divs and about

--- client/src/pages/About/index.tsx
+++ client/src/pages/About/index.tsx
@@ -1,4 +1,4 @@
-import { Container } from "./style";
+import { Container, LinksOfProfiles } from "./style";
 
 export function About() {
     return (
@@ -7,30 +7,36 @@ export function About() {
             <p>
                 Project developed with the objective of learning and evolution. In this project we were able to apply much of our technical knowledge and direct some concepts about React & NodeJs, with the use of libraries in the front-end and back-end.
             </p>
-
-            <h2>Front-End</h2>
-            <ul>
-                <li>React with Vite</li>
-                <li>React Icons</li>
-                <li>TypeScript</li>
-                <li>Context Api</li>
-                <li>Styled-Components</li>
-                <li>React-Router</li>
-                <li>Axios (requisições)</li>
-                <li>React-Hook-Form</li>
-            </ul>
-
-            <h2>Back-End</h2>
-            <ul>
-                <li>NodeJs</li>
-                <li>Express</li>
-                <li>Prisma</li>
-                <li>TypeScript</li>
-                <li>Cors</li>
-                <li>Nodemon</li>
-            </ul>
-
-            <p>System developed by <a target="_blank" href="https://www.linkedin.com/in/jos%C3%A9-murilo/">@Murilo</a> & <a target="_blank" href="https://www.linkedin.com/in/italo-morais/">@Italo</a></p>
+            <div>
+                <h2>Front-End</h2>
+                <ul>
+                    <li>React with Vite</li>
+                    <li>React Icons</li>
+                    <li>TypeScript</li>
+                    <li>Context Api</li>
+                    <li>Styled-Components</li>
+                    <li>React-Router</li>
+                    <li>Axios (requisições)</li>
+                    <li>React-Hook-Form</li>
+                </ul>
+            </div>
+            <div>
+                <h2>Back-End</h2>
+                <ul>
+                    <li>NodeJs</li>
+                    <li>Express</li>
+                    <li>Prisma</li>
+                    <li>TypeScript</li>
+                    <li>Cors</li>
+                    <li>Nodemon</li>
+                </ul>
+            </div>
+            <p>System developed by
+                <LinksOfProfiles>
+                    <a target="_blank" href="https://www.linkedin.com/in/jos%C3%A9-murilo/">@Murilo</a>&
+                    <a target="_blank" href="https://www.linkedin.com/in/italo-morais/">@Italo</a>
+                </LinksOfProfiles>
+            </p>
         </Container>
     )
 }
